c7b38b95862c40232ca3f4c7886883f6ad81d967
Christopher Wong <cgwong1195@gmail.com>
2020-08-08 23:02:52 -0700

    Rename to RP Toolkit since it's more generic

--- rp_toolbox/wsgi.py
+++ rp_toolbox/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dnd_toolbox project.
+WSGI config for rp_toolbox project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dnd_toolbox.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rp_toolbox.settings')
 
 application = get_wsgi_application()
